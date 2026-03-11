8f32fe877ee3dc87185ca0cb79fd5dada8774da9
Gin <hoangngocha11122k6@gmail.com>
2026-03-11 13:50:11 +0700

    Add employee management methods
    Implemented methods to manage employee records including adding, finding, deleting, and displaying employees.

--- cinema/managers/EmployeeManager.java
+++ cinema/managers/EmployeeManager.java
@@ -1,4 +1,37 @@
 package cinema.managers;
-public class EmployeeManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class EmployeeManager {
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public boolean addEmployee(Employee employee){
+        if(findById(employee.getId()) != null){
+            return false;
+        }
+        employeeList.add(employee);
+        return true;
+    }
+    public Employee findById(String id){
+        for(Employee e : employeeList){
+            if(e.getId().equals(id)){
+                return e;
+            }
+        }
+        return null;
+    }
+    public boolean deleteEmployee(String id){
+        Employee e = findById(id);
+        if(e != null){
+            e.deactivateEmployee();
+            return true;
+        }
+        return false;
+    }
+    public void displayAllEmployees(){
+        for(Employee e : employeeList){
+            e.displayInfo();
+            System.out.println("----------------");
+        }
+    }
 }
