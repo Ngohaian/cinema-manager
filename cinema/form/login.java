e8076b41ec193debe3171926157cd2dbb1f0ac5f
Ngohaian <haian082006@gmail.com>
2026-04-03 09:58:58 +0700

    update

--- cinema/form/login.java
+++ cinema/form/login.java
@@ -1,7 +1,6 @@
 
 package cinema.form;
 import javax.swing.JOptionPane;
-
 import cinema.CinemaManager;
 import cinema.managers.EmployeeManager;
 import cinema.models.Employee;
@@ -126,11 +125,6 @@ public class login extends javax.swing.JFrame {
     }                                         
 
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
