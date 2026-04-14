453d79532e4b3f14f4f7eae4bbf76cb1957b2dcb
Ngohaian <haian082006@gmail.com>
2026-04-14 21:06:36 +0700

    update invoice

--- cinema/models/Invoice.java
+++ cinema/models/Invoice.java
@@ -1,1 +1,64 @@
-Windows Explorer
+package cinema.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invoice {
+
+    private String invoiceId;
+    private String customerId;
+    private String invoiceDate;
+    private double totalAmount;
+
+    private List<Ticket> tickets;
+
+    public Invoice() {
+    }
+
+    public Invoice(String invoiceId, String customerId, String invoiceDate, double totalAmount) {
+        this.invoiceId = invoiceId;
+        this.customerId = customerId;
+        this.invoiceDate = invoiceDate;
+        this.totalAmount = totalAmount;
+    }
+    public double calculateTotalAmount(List<Ticket> tickets) {
+        double total = 0;
+        for (Ticket ticket : tickets) {
+            if(ticket.getInvoiceId().equals(this.invoiceId)) {
+                total += ticket.getPrice();
+            }
+        }
+        return total;
+    }
+    public String getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(String invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
