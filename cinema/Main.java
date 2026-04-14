cb4ea3741caca8e58887b373770d0268269fb2c7
Ngohaian <haian082006@gmail.com>
2026-04-14 21:51:31 +0700

    update

--- cinema/Main.java
+++ cinema/Main.java
@@ -1,12 +1,5 @@
 package cinema;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.Connection;
-
 import cinema.DBConnection;
 import cinema.form.LoginFrame; 
 public class Main {
