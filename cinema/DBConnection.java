b094c132cdf3843b155ca0f026e24a1d983c9a5e
Gin <hoangngocha11122k6@gmail.com>
2026-05-19 10:30:00 +0700

    hh

--- cinema/DBConnection.java
+++ cinema/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/cinema_management";
     private static final String USER = "root";
-    private static final String PASSWORD = "Look@tme1112";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
