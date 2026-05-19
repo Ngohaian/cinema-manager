57af4cff17cb7c53edcc1a8a087da3405e6103ae
Ngohaian <haian082006@gmail.com>
2026-05-19 11:37:15 +0700

    up

--- cinema/CinemaManager.java
+++ cinema/CinemaManager.java
@@ -7,14 +7,14 @@ public class CinemaManager {
     public CinemaManager(){
     }
     public void login(){
-        try {
-        com.formdev.flatlaf.FlatLightLaf.setup(); 
-        
-    } catch (Exception e) {
-        System.err.println("Không thể khởi tạo FlatLaf");
+            try {
+            com.formdev.flatlaf.FlatLightLaf.setup(); 
+            
+        } catch (Exception e) {
+            System.err.println("Không thể khởi tạo FlatLaf");
+        }
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        }
     }
-        LoginFrame loginFrame = new LoginFrame();
-        loginFrame.setVisible(true);
-    }
-}
 
