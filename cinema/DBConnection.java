04f937fec01923082a5a46263ba875675b708d8a
Ngohaian <haian082006@gmail.com>
2026-05-24 22:42:08 +0700

    update

--- cinema/DBConnection.java
+++ cinema/DBConnection.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-   private static final String URL = "jdbc:mysql://localhost:3306/cinemamanager?useSSL=false&serverTimezone=GMT%2B7";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Look@tme1112";
+    private static final String URL = "jdbc:mysql://localhost:3306/cinemamanager?useSSL=false&serverTimezone=GMT%2B7";
+    private static final String USER = "test";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() {
         try {
