ddb88c0afd04d553f8d480ff510e76b11767505e
Ngohaian <haian082006@gmail.com>
2026-06-09 23:28:37 +0700

    update

--- cinema/DBConnection.java
+++ cinema/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DBConnection {
    private static final String URL = "jdbc:mysql://localhost:3306/cinemamanager?useSSL=false&serverTimezone=GMT%2B7";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() {
         try {
