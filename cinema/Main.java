941b38d9e04754a4809f3f0f348f17d67e435bba
Ngohaian <haian082006@gmail.com>
2026-06-10 21:06:35 +0700

    update

--- cinema/Main.java
+++ cinema/Main.java
@@ -1,7 +1,6 @@
 package cinema;
 public class Main {
     public static void main(String[] args) {
-        new cinema.dao.EmployeeDAO().migratePasswordsToBcrypt();        // dòng này lúc chạy test thì chạy trước 1 lần rồi xóa đi nhé
         CinemaManager cinemaManager = new CinemaManager();
         cinemaManager.login();
     }
